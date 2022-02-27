f9ab65b0902afcb653adefded10606a54d2c67fb
Amanda Dell <amhernandez1163@gmail.com>
2022-02-27 00:58:35 -0600

    updated vaiables in validate password - working on random/passwordlength

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // Assignment code here 
 
+var passwordLength = [];
 // array upper
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // array lower
@@ -8,104 +9,81 @@ var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 // array spec charc
 var specialCharacters = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
-
-// randomize vars
-
-var randomUppercase = Math.floor(Math.random() * [uppercase]);
-
-
+// catch all for vaiables
+var characters = []
 
 function validatePassword () {
-  //when user clicks button - prented w a series of prompts
+  //when user clicks button - prented w a series of prompts - BEGIN PROMPTS
 
   // prompt for length of password 8 min - 128 max // text input
-  var characterLength = window.prompt("Total Character Count? Please enter a value from 8 - 128")
+  var characterLengthPrompt = window.prompt("Total Character Count? Please enter a value from 8 - 128")
   
   // parameters for invalid responses - window alert then return to beginning
-  const isInteger = characterLength;  
-  if ((characterLength === "" || characterLength > 128 || characterLength < 8 || characterLength != isInteger)) {
+  const isInteger = characterLengthPrompt;  
+  if ((characterLengthPrompt === "" || characterLengthPrompt > 128 || characterLengthPrompt < 8 || characterLengthPrompt != isInteger)) {
       window.alert("Please enter a valid entry.");
       return validatePassword();
     } else {
-      console.log("Your character count is " + characterLength);
+      characters = characters.concat(passwordLength)
+      console.log("Your character count is " + characterLengthPrompt);
       } 
-  // prompt = uppercase? // confirm? - variables set up above
-  var uppercase = window.confirm("Would you like uppercase values in your password?");
+  // prompt = uppercase // confirm 
+  var uppercasePrompt = window.confirm("Would you like uppercase values in your password?");
   
-    if (uppercase) {
-      console.log(uppercase);
+    if (uppercasePrompt) {
+      characters = characters.concat(uppercase);
     } else {
-      console.log("Uppercase not wanted");
+      console.log("Uppercase value not wanted");
     }
-  // prompt = lowercase? // confirm? - variables set up above
-  var lowercase = window.confirm("Would you like lowercase values in your password?");
+  // prompt = lowercase // confirm 
+  var lowercasePrompt = window.confirm("Would you like lowercase values in your password?");
       
-    if (lowercase) {
-      console.log(lowercase);
+    if (lowercasePrompt) {
+      characters = characters.concat(lowercase);
     } else {
-      console.log("Lowercase not wanted");
+      console.log("Lowercase value not wanted");
     }
 
-  // prompt = numeric // confirm? - variables set up above  
-  var numeric = window.confirm("Would you like numeric values in your password?");
+  // prompt = numeric // confirm  
+  var numericPrompt = window.confirm("Would you like numeric values in your password?");
   
-    if (numeric) {
-      console.log(numeric);
+    if (numericPrompt) {
+      characters = characters.concat(numeric);
     } else {
-      console.log("Numeric not wanted");
+      console.log("Numeric values not wanted");
     }
 
-  // prompt = special characters //  confirm? - variables set up above
-  var specialCharacters = window.confirm("Would you like special character values in your password?");
-    if (specialCharacters){
-      console.log(specialCharacters);
+  // prompt = special characters //  confirm 
+  var specialCharactersPrompt = window.confirm("Would you like special character values in your password?");
+    if (specialCharactersPrompt){
+      characters = characters.concat(specialCharacters);
     }  else {
-      console.log("Special Characters not wanted");
+      console.log("Special Characters values not wanted");
     } 
     // prompt = must be at least one selected 
-    if (uppercase == false && lowercase == false && numeric == false && specialCharacters == false) {
+    if (uppercasePrompt == false && lowercasePrompt == false && numericPrompt == false && specialCharactersPrompt == false) {
       window.alert("You must choose at least one value.")
       validatePassword();
     }
 
-    while (uppercase === true) {
-      console.log(randomUppercase)
-      break;
-    }
-  
-    // based off inputs, assign a random value from the different var families
-   // if (uppercase === true && lowercase === true && numeric === true && specialCharacters === true){
-   //   return 
-   // }
+    validatePassword();
+ // this return will pass to var password and display on the screen
+  //return "Gererated password will go here";
 
-  validatePassword();
-  
-  
-
-
-  
-
-
-
-
-
-
-  // this return will pass to var password and display on the screen
-  return "Gererated password will go here";
 };
-
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = validatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  var randomPassword = "";
+    // for loop to generate new password w values selected
+      for (i = 0; i < passwordLength; i++) {
+        randomPassword = randomPassword + characters[Math.floor(Math.random() * characters.length)];
+      }
+      writePassword();
+      return randomPassword;
 }
 
 // Add event listener to generate button
